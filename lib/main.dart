cb55b0bce52101b1cd6268c08160e2484ac4c8cb
TotoB12 <totobeliard@outlook.com>
2023-10-05 21:42:44 -0400

    help

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
 import 'package:flutter/services.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   OpenFoodAPIConfiguration.userAgent = UserAgent(name: 'Provenance');
@@ -40,9 +41,12 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String barcode = '';
-  Future<String>? productNameFuture;
+  Future<Product>? productFuture;
   int _selectedIndex = 1;
   bool isFlashOn = false;
+  bool isCameraFlipped = false;
+  String errorMessage =
+      'Please try again. Sorry, but either this product is not in the database, or the scan was unsuccessful.';
   MobileScannerController cameraController = MobileScannerController(
     formats: [BarcodeFormat.ean13, BarcodeFormat.upcA],
   );
@@ -54,12 +58,11 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(
           widget.title,
           style: const TextStyle(
-            // fontSize: 24,
             fontFamily: 'Poly',
             fontWeight: FontWeight.w700,
           ),
         ),
-        backgroundColor: Color(0xFF262626),
+        backgroundColor: const Color(0xFF262626),
         toolbarHeight: 47.0,
       ),
       body: IndexedStack(
@@ -79,16 +82,13 @@ class _MyHomePageState extends State<MyHomePage> {
                         onDetect: (capture) async {
                           final List<Barcode> barcodes = capture.barcodes;
                           for (final barcode in barcodes) {
-                            // if (barcode.format == BarcodeFormat.ean13 || barcode.format == BarcodeFormat.upcA) {
                             if (barcode.rawValue != this.barcode) {
                               setState(() {
                                 this.barcode = barcode.rawValue ?? '';
-                                productNameFuture =
-                                    getProductInfo(this.barcode);
+                                productFuture = getProductInfo(this.barcode);
                               });
                               HapticFeedback.heavyImpact();
                             }
-                            // }
                           }
                         },
                       ),
@@ -97,15 +97,19 @@ class _MyHomePageState extends State<MyHomePage> {
                       bottom: 10.0,
                       left: 10.0,
                       child: Container(
-                        decoration: BoxDecoration(
+                        decoration: const BoxDecoration(
                           color: Colors.white,
                           shape: BoxShape.circle,
                         ),
                         child: IconButton(
-                          icon: Icon(Icons.flip_camera_ios),
+                          icon: const Icon(Icons.flip_camera_ios),
                           onPressed: () {
                             cameraController.switchCamera();
                             HapticFeedback.lightImpact();
+                            setState(() {
+                              isFlashOn = false;
+                              isCameraFlipped = !isCameraFlipped;
+                            });
                           },
                         ),
                       ),
@@ -113,54 +117,157 @@ class _MyHomePageState extends State<MyHomePage> {
                     Positioned(
                       bottom: 10.0,
                       right: 10.0,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: isFlashOn ? Colors.yellow : Colors.white,
-                          shape: BoxShape.circle,
-                        ),
-                        child: IconButton(
-                          icon: Icon(Icons.flash_on),
-                          onPressed: () {
-                            cameraController.toggleTorch();
-                            HapticFeedback.lightImpact();
-                            setState(() {
-                              isFlashOn = !isFlashOn;
-                            });
-                          },
-                        ),
-                      ),
+                      child: isCameraFlipped
+                          ? Container()
+                          : Container(
+                              decoration: BoxDecoration(
+                                color: isFlashOn ? Colors.yellow : Colors.white,
+                                shape: BoxShape.circle,
+                              ),
+                              child: IconButton(
+                                icon: const Icon(Icons.flash_on),
+                                onPressed: () {
+                                  cameraController.toggleTorch();
+                                  HapticFeedback.lightImpact();
+                                  setState(() {
+                                    isFlashOn = !isFlashOn;
+                                  });
+                                },
+                              ),
+                            ),
                     ),
                   ],
                 ),
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.7,
-                  // color: Colors.white,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: FutureBuilder<String>(
-                      future: productNameFuture,
-                      builder: (BuildContext context,
-                          AsyncSnapshot<String> snapshot) {
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return const Center(
-                              child:
-                                  CircularProgressIndicator()); // Wrapped in Center
-                        } else if (snapshot.hasError) {
-                          return Text('Error: ${snapshot.error}');
-                        } else {
-                          return Text(
-                            '\n\n\nScanned Barcode: $barcode\n\nProduct Name: ${snapshot.data}',
-                            style: const TextStyle(
-                              fontSize: 24,
-                              fontFamily: 'Poly',
-                              fontWeight: FontWeight.w700,
+                Expanded(
+                  child: FutureBuilder<Product>(
+                    future: productFuture,
+                    builder: (BuildContext context,
+                        AsyncSnapshot<Product> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(child: CircularProgressIndicator());
+                      } else if (snapshot.hasError) {
+                        return Text(errorMessage);
+                      } else {
+                        if (snapshot.hasData) {
+                          return SingleChildScrollView(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Card(
+                                  child: ListTile(
+                                    leading: Container(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.2, // 20% of screen width
+                                      child:
+                                          snapshot.data?.imageFrontUrl != null
+                                              ? Image.network(
+                                                  snapshot.data!.imageFrontUrl!,
+                                                  fit: BoxFit.cover,
+                                                )
+                                              : const Icon(Icons.shopping_cart,
+                                                  size: 24.0),
+                                    ),
+                                    title: Text(
+                                      '${snapshot.data?.productName ?? 'Unknown Product'}',
+                                      style: const TextStyle(
+                                        fontSize: 24,
+                                        fontFamily: 'Poly',
+                                        fontWeight: FontWeight.w700,
+                                      ),
+                                    ),
+                                    subtitle: Text(
+                                      'Brands: ${snapshot.data?.brands ?? 'Unknown'}',
+                                      style: const TextStyle(
+                                        fontSize: 20,
+                                        fontFamily: 'Poly',
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                if (snapshot.data?.ecoscoreGrade != null &&
+                                    snapshot.data!.ecoscoreGrade !=
+                                        'not-applicable' &&
+                                    snapshot.data!.ecoscoreGrade != 'unknown')
+                                  Card(
+                                    child: ListTile(
+                                      leading: Container(
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.4, // 40% of screen width
+                                        child: SvgPicture.asset(
+                                          'assets/images/ecoscore-${snapshot.data!.ecoscoreGrade}.svg',
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                      title: Text(
+                                        'EcoScore: ${snapshot.data?.ecoscoreGrade ?? 'Unknown'}',
+                                        style: const TextStyle(
+                                          fontSize: 20,
+                                          fontFamily: 'Poly',
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                if (snapshot.data?.novaGroup != null &&
+                                    snapshot.data!.novaGroup !=
+                                        'not-applicable')
+                                  Card(
+                                    child: ListTile(
+                                      leading: Container(
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.4, // 40% of screen width
+                                        child: SvgPicture.asset(
+                                          'assets/images/nova-group-${snapshot.data!.novaGroup}.svg',
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                      title: Text(
+                                        'Nova Group: ${snapshot.data?.novaGroup ?? 'Unknown'}',
+                                        style: const TextStyle(
+                                          fontSize: 20,
+                                          fontFamily: 'Poly',
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                if (snapshot.data?.nutriscore != null &&
+                                    snapshot.data!.nutriscore !=
+                                        'not-applicable')
+                                  Card(
+                                    child: ListTile(
+                                      leading: Container(
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.4, // 40% of screen width
+                                        child: SvgPicture.asset(
+                                          'assets/images/nutriscore-${snapshot.data!.nutriscore}.svg',
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                      title: Text(
+                                        'NutriScore: ${snapshot.data?.nutriscore ?? 'Unknown'}',
+                                        style: const TextStyle(
+                                          fontSize: 20,
+                                          fontFamily: 'Poly',
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                // Add more fields as needed
+                              ],
                             ),
-                            textAlign: TextAlign.center,
                           );
+                        } else {
+                          return Text(errorMessage);
                         }
-                      },
-                    ),
+                      }
+                    },
                   ),
                 ),
               ],
@@ -225,19 +332,25 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Future<String> getProductInfo(String barcode) async {
+  Future<Product> getProductInfo(String barcode) async {
     ProductQueryConfiguration config = ProductQueryConfiguration(
       barcode,
       language: OpenFoodFactsLanguage.ENGLISH,
-      fields: [ProductField.NAME],
+      fields: [
+        ProductField.ALL,
+      ],
       version: ProductQueryVersion.v3,
     );
     try {
       ProductResultV3 result = await OpenFoodAPIClient.getProductV3(config);
-      return result.product?.productName ?? 'Unknown';
+      if (result.product != null) {
+        return result.product!;
+      } else {
+        throw Exception('Product not found');
+      }
     } catch (e) {
-      print('Failed to get product info: $e');
-      return 'Error: Failed to get product info: $e';
+      debugPrint('Failed to get product info: $e');
+      throw Exception('Failed to get product info: $e');
     }
   }
 }
